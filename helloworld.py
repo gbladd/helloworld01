0538712a7bbf6c369c400dc65946785e469a3b78
Gabe Ladd <gabe@americanaerospace.com>
2016-08-16 17:49:10 -0400

    Add Main Check structure
    Added the check to see if this is the main function being exicuted by
    the python interperater.

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,16 @@ Created on Tue Aug 16 15:49:40 2016
 
 @author: gladd
 """
+    
+if __name__ == "__main__":
+    main()
+    print("================= End of Script ====================")
+    
 
-print("\n\n\nHello World\n\n\n")
 
-print("================= End of Script ====================")
+
+
+def main():
+    #this is the main function
+    print("This is the main function")
+    print("\n\n\nHello World\n\n\n")
